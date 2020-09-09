80c55ee98bc6649dc4cb0af5f5857bae36acd511
Guilherme Roberto Ferreira Santos <guilherme.rfs00@gmail.com>
2020-09-09 09:48:24 -0300

    Primeiro commit

---
+++ src/br/com/cod3r/cm/Aplicacao.java
@@ -0,0 +1,10 @@
+package br.com.cod3r.cm;
+
+import br.com.cod3r.cm.modelo.Tabuleiro;
+
+public class Aplicacao {
+	public static void main(String[] args) {
+		Tabuleiro tabuleuiro = new Tabuleiro(6, 6, 6);
+		System.out.println(tabuleuiro);
+	}
+}
